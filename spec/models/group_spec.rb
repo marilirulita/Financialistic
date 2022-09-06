8f55904e577321ab4ba5e4beb4dcbb956694d9c7
mar <marilirulita@gmail.com>
2022-09-06 13:02:40 -0700

    add tests and validations for model group

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    
+    subject!(:user) { User.create(name: "mar", email: "mar@email.com", password: "123456") }
+    subject!(:group) { Group.create(name: "home", icon: "https://upload.wikimedia.org/wikipedia/commons/9/97/Circle-icons-art.svg", user: user) }
+
+    it "requires a name" do
+      group.name = nil
+      expect(group).to_not be_valid
+    end
+
+    it "requires an icon" do
+      group.icon = nil
+      expect(group).to_not be_valid
+    end
+  end
+  
 end
