7aa1eeaccbb9832d14f65c50ea7ed565537d1669
mar <marilirulita@gmail.com>
2022-09-06 12:57:55 -0700

    add tests and validations to move

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,4 +1,6 @@
 class Move < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: "author_id"
   has_many :groups
+  validates :name, presence: true
+  validates :amount, presence: true
 end
--- spec/models/move_spec.rb
+++ spec/models/move_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Move, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    
+    subject!(:user) { User.create(name: "mar", email: "mar@email.com", password: "123456") }
+    subject!(:move) { Move.create(name: "food", amount: "20.5", author_id: user.id) }
+
+    it "requires a name" do
+      move.name = nil
+      expect(move).to_not be_valid
+    end
+
+    it "requires an amount" do
+      move.amount = nil
+      expect(move).to_not be_valid
+    end
+  end
 end
