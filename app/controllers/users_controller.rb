1264bd95fc064252c3ec418ee45509b535b57ecd
mar <marilirulita@gmail.com>
2022-09-06 15:03:57 -0700

    remove unecesary users code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,4 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-
-  # GET /users or /users.json
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :groups
   resources :moves
 
-  root to: "users#index"
+  root to: "groups#index"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
