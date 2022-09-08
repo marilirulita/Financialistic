03e76c41788ccf69fc07c3ec80cc7f287b7f7c08
mar <marilirulita@gmail.com>
2022-09-08 09:54:20 -0700

    add icons for categories

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,5 @@
 module GroupsHelper
+  def icons_array
+    ['pot-food', 'bowl-food', 'burger-soda', 'utensils', 'cart-shopping-fast', 'house-crack', 'bath', 'clothes-hanger', 'stethoscope']
+  end
 end
