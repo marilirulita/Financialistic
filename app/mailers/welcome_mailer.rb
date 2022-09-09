7037340d4089c11f4c041a1ebd05aec7d3dee330
mar <marilirulita@gmail.com>
2022-09-09 11:01:13 -0700

    add test in new mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,6 +2,6 @@ class WelcomeMailer < ApplicationMailer
   def welcome_send(user)
     @user = user
    # mail(to: @user.email, subject: 'Welcome to the Move Tracker!')
-    mail to: @user.email, subject: 'Welcome to the Move Tracker!', from: 'example@email.com'
+    mail to: @user.email, subject: 'Welcome to the Move Tracker!', from: 'no-reply-financialistic-app@outlook.com'
   end
 end
